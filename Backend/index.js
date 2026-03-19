5c21dd0fcf09d18453e160e4533cf6581d69711e
sanrosheed <sanrosheed01@gmail.com>
2026-03-19 08:39:11 +0100

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,14 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 dotenv.config();
